7c03d93a204474eda49e57bbd699cb0c46d24bfe
Avery Pace <blainepace@gmail.com>
2020-01-14 12:24:25 -0500

    Add new and create actions to WineController

--- app/controllers/wine_controller.rb
+++ app/controllers/wine_controller.rb
@@ -2,4 +2,16 @@ class WineController < ApplicationController
   def index
     @wines = Wine.all
   end
+
+  def new
+    @wine = Wine.new
+  end
+
+  def create
+    params.permit!
+    @wine = Wine.new(params[:wine])
+    if @wine.save
+      redirect_to wine_index_url
+    end
+  end
 end
