197fe95547cd41aab010e5c7cf5f05ddb779e2b6
Avery Pace <blainepace@gmail.com>
2020-01-14 11:53:02 -0500

    Extract get_all_wines out of SplashController#index

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -2,11 +2,15 @@ require 'httparty'
 
 class SplashController < ApplicationController
   def index
+    get_all_wines
+    @response
+  end
+
+  def get_all_wines
     base_uri = "https://api.globalwinescore.com/globalwinescores/latest/"
     @response = HTTParty.get(base_uri, headers: {
       'Authorization' => 'Token f25fdb401cbcc2c4647250073914faff37e3bd97',
       'Content-Type'  => 'application/json'
     })
-    logger.info @response.class
   end
-end
+end   
